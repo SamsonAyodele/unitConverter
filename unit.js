405736a80f54d2e2d37f433ca76883f6bad253fb
Samson Ayodele <samson.idowu@bankly.ng>
2023-12-20 22:07:21 +0100

    updated

--- unit.js
+++ unit.js
@@ -1,10 +1,6 @@
 const input = document.getElementById("num");
 
-
-// console.log(updateValue);
-
 const error = document.getElementById("error");
-
 
 const meterHtml = document.getElementById("meter");
 const feetHtml = document.getElementById("feet");
@@ -21,78 +17,97 @@ const poundHtml = document.getElementById("pound");
 const kiloHtml2 = document.getElementById("kilo2");
 const poundHtml2 = document.getElementById("pound2");
 
-input.addEventListener('input', updateValue);
+input.addEventListener("input", updateValue);
 
 function updateValue(e) {
-    newValue =  e.srcElement.value;
-    convertNumber(newValue);
+  const newValue = parseFloat(e.target.value);
+  convertNumber(newValue);
 }
 
-
-// There are 3.28 foot in a meter 
+// There are 3.28 foot in a meter
 // 1 foot = 3.28meter i.e 1 meter * 3.28foot
-// 1 meter = 1 foot / 3.28 , 1 foot / 3.28 = 0.305 meter 
+// 1 meter = 1 foot / 3.28 , 1 foot / 3.28 = 0.305 meter
 function feetToMeter(feet) {
-    const meter = feet / 3.28084;
-    console.log('conversion of 1 ${feet} to meter is: ${meter.toFixed(3)} meter.');
-    feetHtml.textContent = feet;
-    meterHtml.textContent = meter.toFixed(3);
+  const meter = feet / 3.28084;
+  console.log(
+    `conversion of ${feet} feet to meter is: ${meter.toFixed(3)} meter.`
+  );
+  feetHtml.textContent = feet;
+  meterHtml.textContent = meter.toFixed(3);
 }
-
 
 function meterToFeet(meter) {
-    const feet = meter * 3.2808;
-    console.log('conversion of 1 ${meter} to feet is: ${feet.toFixed(3)} feets.');
-    feetHtml2.textContent = feet.toFixed(3);
-    meterHtml2.textContent = meter;
+  const feet = meter * 3.2808;
+  console.log(`conversion of ${meter} meter to feet is: ${feet.toFixed(3)} feet.`);
+  meterHtml2.textContent = meter;
+  feetHtml2.textContent = feet.toFixed(3);
 }
 
 function literToGallon(liter) {
-    const gallon = liter * 0.264172;
-    console.log('conversion of 1 $(liter) to gallon is : $(gallon.toFixed(3)) gallon.');
-    literHtml.textContent = liter;
-    gallonHtml.textContent = gallon.toFixed(3);
+  const gallon = liter * 0.264172;
+  console.log(
+    `conversion of ${liter} liters to gallon is : ${gallon.toFixed(3)} gallons.`
+  );
+  literHtml.textContent = liter;
+  gallonHtml.textContent = gallon.toFixed(3);
 }
 
 function gallonToLiter(gallon) {
-    const liter = gallon / 0.264172;
-    console.log('conversion of 1 $(gallon) to liter is : $(liter.toFixed(3)) liter.');
-    gallonHtml2.textContent = gallon;
-    literHtml2.textContent = liter.toFixed(3);
+  const liter = gallon / 0.264172;
+  console.log(
+    `conversion of ${gallon} gallons to liter is : ${liter.toFixed(3)} liters.`
+  );
+  gallonHtml2.textContent = gallon;
+  literHtml2.textContent = liter.toFixed(3);
 }
 
 function kiloToPound(kilo) {
-    const pound = kilo * 2.2046;
-    console.log('conversion of 1 $(kilo) to pound is : $(pound.toFixed(3)) pound.');
-    kiloHtml.textContent = kilo;
-    poundHtml.textContent = pound.toFixed(3);
+  const pound = kilo * 2.2046;
+  console.log(
+    `conversion of ${kilo} kilo to pound is : ${pound.toFixed(3)} pound.`
+  );
+  kiloHtml.textContent = kilo;
+  poundHtml.textContent = pound.toFixed(3);
 }
 
 function poundToKilo(pound) {
-    const kilo = pound / 0.2046;
-    console.log('conversion of 1 $(pound) to kilo is : $(kilo.toFixed(3)) kilo.');
-    poundHtml2.textContent = pound;
-    kiloHtml2.textContent = kilo.toFixed(3);
+  const kilo = pound / 0.2046;
+  console.log(`conversion of ${pound} pound to kilo is : ${kilo.toFixed(3)} kilo.`);
+  poundHtml2.textContent = pound;
+  kiloHtml2.textContent = kilo.toFixed(3);
 }
 
 function convertNumber(value) {
-    const newValueNum = newValue(value);
 
-    if (isNaN (newValueNum)) {
-        console.log("Not a Number!");
-        error.textContent = "Not a Number";
-    } else {
-        error.textContent = "";
+  if (isNaN(value)) {
+    console.log("Not a Number!");
+    error.textContent = "Not a Number";
+  } else {
+    error.textContent = "";
 
-        meterToFeet(newValueNum);
-        feetToMeter(newValueNum);
+    meterToFeet(value);
+    feetToMeter(value);
 
-        literToGallon(newValueNum);
-        gallonToLiter(newValueNum);
+    literToGallon(value);
+    gallonToLiter(value);
 
-        poundToKilo(newValueNum);
-        kiloToPound(newValueNum);
-
-    }
+    poundToKilo(value);
+    kiloToPound(value);
+  }
 }
+
+// function convertNumber(value) {
+//   if (isNaN(value)) {
+//       console.log("Not a Number!");
+//       error.textContent = "Not a Number";
+//   } else {
+//       error.textContent = "";
+//       meterToFeet(value);
+//       feetToMeter(value);
+//       literToGallon(value);
+//       gallonToLiter(value);
+//       poundToKilo(value);
+//       kiloToPound(value);
+//   }
+// }
 // console.log(convertNumber(input.value))
